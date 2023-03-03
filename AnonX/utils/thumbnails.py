e14a0091cdd7c7e6c276c0421cf2ac28ae21b63f
Kizzie <110618945+Anonymous56Risk@users.noreply.github.com>
2023-03-03 11:13:52 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -124,7 +124,7 @@ async def gen_thumb(videoid, user_id):
         para = textwrap.wrap(title, width=32)
         try:
             draw.text(
-                (450, 25),
+                (480, 25),
                 f"RMGXMUSIC",
                 fill="white",
                 stroke_width=3,
